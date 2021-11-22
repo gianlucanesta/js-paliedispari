5deb19ddca37e0812961da8e94ad32b1f52e4f5d
gianlucanesta <gianluca.nesta@gmail.com>
2021-11-22 16:40:52 +0100

    completed

--- Palindromo/js/main.js
+++ Palindromo/js/main.js
@@ -1,3 +1,33 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
+
+// Chiedo all' utente di inserire una parola
+let userWord = prompt('Inserisci una parola: ');
+// console.log(userWord);
+
+
+// Dichiaro la funzione e gli assegno come parametro la stringa inserita dall'utente
+function palindrome(userWord) {
+    // Rendo la stringa tutta minuscola
+    const lowerText = userWord.toLowerCase();
+    // console.log(lowerText);
+
+    // Divido la stringa poi la inverto e la riconpongo
+    const invertedText = lowerText.split('').reverse().join('');
+    // console.log(invertedText);
+
+
+    if(lowerText === invertedText) {
+        const isPalindrome = "Hai inserito una parola Palindroma";
+        alert(isPalindrome);
+    } else {
+        const isNotPalindrome = "Hai inserito una parola non Palindroma";
+        alert(isNotPalindrome);
+    }
+}
+
+// Richiamo la funzione
+palindrome(userWord)
+
+
